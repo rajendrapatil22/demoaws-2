9a7f2fe0ba808ebb91524537588d6cc589f0f73a
rajendrap <rajendrap@BXLVNRAJENDRAP>
2021-06-23 06:27:12 +0530

    ss

--- src/test/java/com/in28minutes/rest/webservices/restfulwebservices/ItemTests.java
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/ItemTests.java
@@ -1,6 +1,5 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
-
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -33,58 +32,59 @@ import com.in28minutes.rest.webservices.restfulwebservices.helloworld.ItemReposi
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ItemTests {
-  private static final int CHECKED_ITEM_ID = 1;
-  private static final Item CHECKED_ITEM = new ItemBuilder()
-    .id(CHECKED_ITEM_ID)
-    .checked()
-    .build();
-  private static final Item UNCHECKED_ITEM = new ItemBuilder()
-    .id(2)
-    .checked()
-    .build();
-  private static final Item NEW_ITEM = new ItemBuilder()
-    .checked()
-    .build();
-  @Test
+	private static final int CHECKED_ITEM_ID = 1;
+	private static final Item CHECKED_ITEM = new ItemBuilder().id(CHECKED_ITEM_ID).checked().build();
+	private static final Item UNCHECKED_ITEM = new ItemBuilder().id(2).checked().build();
+	private static final Item NEW_ITEM = new ItemBuilder().checked().build();
 
-  public void getByIdKO() {
-  Response response=  given().when().get("http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items").then().extract().response();
-  assertEquals(200, response.getStatusCode());
-    
-  }
-  //RestAssured get All Item
-	
-	 @Test public void testItemList() { Response resp=(Response)
-	 RestAssured.given().when().get(
-	 "http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items").then().
-	 extract().response(); assertEquals(200, resp.getStatusCode()); }
-	 
-	 //Save Data to Item
-	
-	 @Test public void testSaveItem() { Item item=new Item(); item.setId(3);
-	 item.setChecked(true); item.setDescription("data");
-	 
-	 Response response =
-	 RestAssured.given().contentType(ContentType.JSON).header("Content-Type",
-	 ContentType.JSON) .body(new Gson().toJson(item)).post(
-	 "http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items").then().
-	  extract().response();
-	 
-	 assertEquals(200, response.getStatusCode()); }
-	 
-	 //Update to Item
-	 
-	 @Test public void testupdateItem() { Item item=new Item(); item.setId(3);
-	 item.setChecked(false); item.setDescription("sdsdsdas");
-	 
-	 Response response = RestAssured.given().baseUri(
-	 "http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items/1").
-	 header("Content-Type", ContentType.JSON) .body(new
-	 Gson().toJson(item)).when().put().then().extract().response();  
-	
-	  
-	  assertEquals(200, response.getStatusCode()); }
-	 
-  
- 
+	@Test
+
+	public void getByIdKO() {
+		Response response = given().when().get("http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items")
+				.then().extract().response();
+		assertEquals(200, response.getStatusCode());
+
+	}
+	// RestAssured get All Item
+
+	@Test
+	public void testItemList() {
+		Response resp = (Response) RestAssured.given().when()
+				.get("http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items").then().extract().response();
+		assertEquals(200, resp.getStatusCode());
+	}
+
+	// Save Data to Item
+
+	@Test
+	public void testSaveItem() {
+		Item item = new Item();
+		item.setId(3);
+		item.setChecked(true);
+		item.setDescription("data");
+
+		Response response = RestAssured.given().contentType(ContentType.JSON).header("Content-Type", ContentType.JSON)
+				.body(new Gson().toJson(item))
+				.post("http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items").then().extract().response();
+
+		assertEquals(200, response.getStatusCode());
+	}
+
+	// Update to Item
+
+	@Test
+	public void testupdateItem() {
+		Item item = new Item();
+		item.setId(3);
+		item.setChecked(false);
+		item.setDescription("sdsdsdas");
+
+		Response response = RestAssured.given()
+				.baseUri("http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items/1")
+				.header("Content-Type", ContentType.JSON).body(new Gson().toJson(item)).when().put().then().extract()
+				.response();
+
+		assertEquals(200, response.getStatusCode());
+	}
+
 }

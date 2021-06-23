17e988fe3796e588035c034cb5d2f7cd91f8fb27
rajendrap <rajendrap@BXLVNRAJENDRAP>
2021-06-23 05:36:04 +0530

    testtt

--- src/test/java/com/in28minutes/rest/webservices/restfulwebservices/ItemTests.java
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/ItemTests.java
@@ -48,45 +48,45 @@ public class ItemTests {
   @Test
 
   public void getByIdKO() {
-  Response response=  given().when().get("http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items/1").then().extract().response();
-  assertEquals(404, response.getStatusCode());
+  Response response=  given().when().get("http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items").then().extract().response();
+  assertEquals(200, response.getStatusCode());
     
   }
   //RestAssured get All Item
-	/*
-	 * @Test public void testItemList() { Response resp=(Response)
-	 * RestAssured.given().when().get(
-	 * "http://demo3-env.eba-vnbfjk9q.us-east-2.elasticbeanstalk.com/items").then().
-	 * extract().response(); assertEquals(200, resp.getStatusCode()); }
-	 * 
-	 * //Save Data to Item
-	 * 
-	 * @Test public void testSaveItem() { Item item=new Item(); item.setId(3);
-	 * item.setChecked(true); item.setDescription("data");
-	 * 
-	 * Response response =
-	 * RestAssured.given().contentType(ContentType.JSON).header("Content-Type",
-	 * ContentType.JSON) .body(new Gson().toJson(item)).post(
-	 * "http://demo3-env.eba-vnbfjk9q.us-east-2.elasticbeanstalk.com/items").then().
-	 * extract().response();
-	 * 
-	 * assertEquals(200, response.getStatusCode()); }
-	 * 
-	 * //Update to Item
-	 * 
-	 * @Test public void testupdateItem() { Item item=new Item(); item.setId(3);
-	 * item.setChecked(false); item.setDescription("sdsdsdas");
-	 * 
-	 * Response response = RestAssured.given().baseUri(
-	 * "http://demo3-env.eba-vnbfjk9q.us-east-2.elasticbeanstalk.com/items/1").
-	 * header("Content-Type", ContentType.JSON) .body(new
-	 * Gson().toJson(item)).when().put().then().extract().response(); // Response
-	 * resp=(Response)
-	 * RestAssured.given().when().get("http://localhost:8086/items/").then().extract
-	 * ().response();
-	 * 
-	 * assertEquals(200, response.getStatusCode()); }
-	 */
+	
+	 @Test public void testItemList() { Response resp=(Response)
+	 RestAssured.given().when().get(
+	 "http://demo3-env.eba-vnbfjk9q.us-east-2.elasticbeanstalk.com/items").then().
+	 extract().response(); assertEquals(200, resp.getStatusCode()); }
+	 
+	 //Save Data to Item
+	
+	 @Test public void testSaveItem() { Item item=new Item(); item.setId(3);
+	 item.setChecked(true); item.setDescription("data");
+	 
+	 Response response =
+	 RestAssured.given().contentType(ContentType.JSON).header("Content-Type",
+	 ContentType.JSON) .body(new Gson().toJson(item)).post(
+	 "http://demo3-env.eba-vnbfjk9q.us-east-2.elasticbeanstalk.com/items").then().
+	  extract().response();
+	 
+	 assertEquals(200, response.getStatusCode()); }
+	 
+	 //Update to Item
+	 
+	 @Test public void testupdateItem() { Item item=new Item(); item.setId(3);
+	 item.setChecked(false); item.setDescription("sdsdsdas");
+	 
+	 Response response = RestAssured.given().baseUri(
+	 "http://demo3-env.eba-vnbfjk9q.us-east-2.elasticbeanstalk.com/items/1").
+	 header("Content-Type", ContentType.JSON) .body(new
+	 Gson().toJson(item)).when().put().then().extract().response();  Response
+	 resp=(Response)
+	 RestAssured.given().when().get("http://localhost:8086/items/").then().extract
+	  ().response();
+	  
+	  assertEquals(200, response.getStatusCode()); }
+	 
   
  
 }

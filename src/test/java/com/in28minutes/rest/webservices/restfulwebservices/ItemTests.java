08b88d3bd25c985ab6bb4eeffa3d16ff6d5747c9
rajendrap <rajendrap@BXLVNRAJENDRAP>
2021-06-27 23:28:56 +0530

    ds

--- src/test/java/com/in28minutes/rest/webservices/restfulwebservices/ItemTests.java
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/ItemTests.java
@@ -3,14 +3,20 @@ package com.in28minutes.rest.webservices.restfulwebservices;
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.hibernate.mapping.Map;
 import org.json.JSONObject;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.google.gson.Gson;
@@ -23,7 +29,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
-@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = ItemTests.class)
 @SpringBootTest
 public class ItemTests {
 	private static final int CHECKED_ITEM_ID = 1;
@@ -32,61 +38,108 @@ public class ItemTests {
 	private static final Item NEW_ITEM = new ItemBuilder().checked().build();
 	// RestAssured get All Item
 HashMap<String, String> map=new HashMap<String, String>();
-	/*
-	 * @Test public void databaseexistOrNot() { Response response = given().when()
-	 * .get(
-	 * "http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/h2-console/").
-	 * then().extract() .response();
-	 * 
-	 * assertEquals(200, response.getStatusCode());
-	 * 
-	 * }
-	 */
-	@Test
-	public void planetsCheck() throws Exception {
-	    Response response = given()
-	            .filter(new AllureRestAssured())
-	            .when()
-	            .get("http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items/100").then()
-	            .extract().response();
-	    map.put("Fail", response.asString());
-	    
-	    new ZetaReport().createReport(map);
-	    Assert.assertEquals(response.statusCode(), 400);
-	
-	}
+Result result=new Result();
+static ArrayList<Result> arr;
+ @org.junit.BeforeClass
+public static void startTest()
+{
+	arr=new ArrayList<Result>();
+}
 
+	 @Test public void databaseexistOrNot() { 
+		 
+	
+		 
+		 Response response = given().when()
+	 .get(
+	 "http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/h2-console/").
+	 then().extract() .response();
+	// result.setId(2);
+	 result.setTestCaseName("databaseexistOrNot");
+		 result.setStatus("Pass");
+	 
+	 result.setResponse("Connected");
+arr.add(result);
+	 assertEquals(200, response.getStatusCode());
+	 
+	 }
+	 
+	
+
+	
+	 @Test public void getAll() { Response response = given().when().get(
+	 "http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items/")
+	 .then().extract().response(); assertEquals(200, response.getStatusCode());
+	// result.setId(1);
+	 result.setTestCaseName("getAll");
+	 result.setStatus("Pass");
+	 
+	 result.setResponse(String.valueOf(response.getStatusCode()));
+arr.add(result);
+	 } // RestAssured get By Item
+	 
+	 @Test public void getById() throws Exception { Response response = given().when().get(
+	"http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items/7")
+	  .then().extract().response(); // typecasting obj to JSONObject 
+	 Item item =
+	 new Gson().fromJson(response.getBody().asString(), Item.class);
+	// result.setId(3);
+	 
+	if(item.getStatus()==true) {
+		 result.setStatus("fail");
+
+		 result.setTestCaseName("getbyId");
+		 result.setResponse("java.lang.AssertionError: expected:<false> but was:<true>\n" + 
+		 		"	at org.junit.Assert.fail(Assert.java:88)\n" + 
+		 		"	at org.junit.Assert.failNotEquals(Assert.java:834)\n" + 
+		 		"	at org.junit.Assert.assertEquals(Assert.java:118)\n" + 
+		 		"	at org.junit.Assert.assertEquals(Assert.java:144)\n" + 
+		 		"	at com.in28minutes.rest.webservices.restfulwebservices.ItemTests.getById(ItemTests.java:88)\n" + 
+		 		"	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n" + 
+		 		
+		 		""); 
+		 arr.add(result); 
+		 //new ZetaReport().createReport(arr);
+		 assertEquals(false, item.getStatus());
+		 
+				 	 	 
+	}else {
+
+		 result.setTestCaseName("getbyId");
+		 result.setResponse(response.asString());
+		 arr.add(result);
+		 assertEquals(true, item.getStatus());
+	// new ZetaReport().createReport(arr);
+	 }
+	 }
+	 
+	 // Save Data to Item
+	 
+	  @Test public void testSaveItem() throws Exception { Item item = new Item(); //item.setId(1);
+	  item.setStatus(true); item.setDescription("test case save");
+	 
+	 Response response =
+	 RestAssured.given().contentType(ContentType.JSON).header("Content-Type",
+	 ContentType.JSON) .body(new Gson().toJson(item))
+	 .post("http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items").
+	 then().extract().response();
+	// result.setId(4);
+	 result.setTestCaseName("testSaveItem");
+	 result.setStatus("Pass");
+	 result.setResponse(response.asString()); 
+	 arr.add(result);
+	 assertEquals(200, response.getStatusCode()); 
+	 
+	
+	  }
+	  @AfterClass
+	    public static  void endTest() throws Exception
+	    {
+		  new ZetaReport().createReport(arr);
+	    }
+	 // Update to Item
+	 
 	/*
-	 * @Test public void getAll() { Response response = given().when().get(
-	 * "http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items/")
-	 * .then().extract().response(); assertEquals(200, response.getStatusCode());
-	 * 
-	 * } // RestAssured get By Item
-	 * 
-	 * @Test public void getById() { Response response = given().when().get(
-	 * "http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items/7")
-	 * .then().extract().response(); // typecasting obj to JSONObject Item item =
-	 * new Gson().fromJson(response.getBody().asString(), Item.class);
-	 * 
-	 * assertEquals(true, item.getStatus());
-	 * 
-	 * }
-	 * 
-	 * // Save Data to Item
-	 * 
-	 * @Test public void testSaveItem() { Item item = new Item(); //item.setId(1);
-	 * item.setStatus(true); item.setDescription("test case save");
-	 * 
-	 * Response response =
-	 * RestAssured.given().contentType(ContentType.JSON).header("Content-Type",
-	 * ContentType.JSON) .body(new Gson().toJson(item))
-	 * .post("http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items").
-	 * then().extract().response();
-	 * 
-	 * assertEquals(200, response.getStatusCode()); }
-	 * 
-	 * // Update to Item
-	 * 
 	 * @Test public void testupdateItem() { Item item = new Item(); item.setId(8);
 	 * item.setStatus(true); item.setDescription("status updated to true");
 	 * 
@@ -94,8 +147,8 @@ HashMap<String, String> map=new HashMap<String, String>();
 	 * "http://demo4-env.eba-ivkwtadj.us-east-2.elasticbeanstalk.com/items/8")
 	 * .header("Content-Type", ContentType.JSON).body(new
 	 * Gson().toJson(item)).when().put().then().extract() .response();
-	 * 
-	 * assertEquals(200, response.getStatusCode()); }
+	 * Assert.assertEquals(response.statusCode(), 200); assertEquals(200,
+	 * response.getStatusCode()); }
 	 */
 
 }
--- src/test/java/com/in28minutes/rest/webservices/restfulwebservices/ZetaReport.java
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/ZetaReport.java
@@ -4,6 +4,7 @@ import java.awt.Desktop;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,21 +18,27 @@ public class ZetaReport {
 	private String respFileNameString = "", theJsonPath = "", replaceString = "";
 	private int counterPass = 0, counterFail = 0;
 
-	public void createReport(HashMap<String,String> String ) throws Exception {
+	public void createReport(ArrayList<Result> String ) throws Exception {
 		fileObj = new File("TestReport.html");
 		bufferedWriter = new BufferedWriter(new FileWriter(fileObj));
 		bufferedWriter.write("<html><body><h1>Test Case Result!</h1>");
 		
-		bufferedWriter.write("<!DOCTYPE html>\r\n" + "<html>\r\n" + "<head></head>\r\n" + "<body>\r\n" + "\r\n"
+		bufferedWriter.write("<!DOCTYPE html>\r\n" + "<html>\r\n" + "<head>" + "<style>\r\n" + "table {\r\n"
+				+ "  font-family: arial, sans-serif;\r\n" + "  border-collapse: collapse;\r\n" + "  width: 100%;\r\n"
+				+ "}\r\n" + "\r\n" + "td, th {\r\n" + "  border: 1px solid #dddddd;\r\n" + "  text-align: left;\r\n"
+				+ "  padding: 8px;\r\n" + "}\r\n" + "\r\n" + "tr:nth-child(even) {\r\n"
+				+ "  background-color: #dddddd;\r\n" + "}\r\n" + "</style>\r\n" + "</head>\r\n" + "<body>\r\n" + "\r\n"
 				+ "<table><tr><td><div id=\"piechart\"></div></td><td><div id=\"barchart\"></td></div><table><tr>\r\n"
-				+ "    <th>TestCaseId</th>\r\n" + "    <th>TestCase Description</th>\r\n"
+				+ "    <th>TestCaseId</th>\r\n" +  "<th>Test Name</th>\r\n" +  "    <th>TestCase Description</th>\r\n"
 				+ "    <th>Status</th>\r\n" + "  </tr>");
-		for (int currentRow = 1; currentRow <= String.size(); currentRow++) {
-			
-			if (!"Pass".equalsIgnoreCase(String.get("Pass"))) {
+		for (int currentRow = 0; currentRow <String.size(); currentRow++) {
+			 String status="";
+			if (!"Pass".equalsIgnoreCase(String.get(currentRow).getStatus())) {
 				counterFail = counterFail + 1;
+			status="Fail";
 			} else {
 				counterPass = counterPass + 1;
+				status="Pass";
 			}
 			//respFileName = row.getCell(ACT_RES_FILENAME);
 			//respFileNameString = formatter.formatCellValue(respFileName);
@@ -41,11 +48,12 @@ public class ZetaReport {
 			}
 			replaceString = stringBuilder.toString().replace('/', '\\');
 			
-			bufferedWriter.write("<tr>\r\n" + "<td>" + 1 + "</td>\r\n" + "    <td>"
-					+ "sda" + "</td>\r\n" + " \r\n" + "    <td>"
-					+ "Pass" + "</td>\r\n" + "</tr>\r\n");
+			bufferedWriter.write("<tr>\r\n" + "<td>" + currentRow + "</td>\r\n" + "    <td>"
+					+ String.get(currentRow).getTestCaseName() + "</td>\r\n" + " \r\n" + "    <td>"
+					+ String.get(currentRow).getResponse() + "</td>\r\n" + " \r\n" +"    <td>"
+					+ status + "</td>\r\n" + "</tr>\r\n");
 		}		
-		bufferedWriter.write("<script type=\"text/javascript\" X-Content-Security-Policy: sandbox; default-src 'none'; img-src 'self'; style-src 'self'; src=\"https://www.gstatic.com/charts/loader.js\"></script>\r\n"
+		bufferedWriter.write("<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>\r\n"
 						+ "<script type=\"text/javascript\">\r\n" + "// Load google charts\r\n"
 						+ "google.charts.load('current', {'packages':['corechart']});\r\n"
 						+ "google.charts.setOnLoadCallback(drawChart);\r\n" + "\r\n"
